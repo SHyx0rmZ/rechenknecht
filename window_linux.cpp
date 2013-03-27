036d88630d4898c2a2101995e9718f34fff6e9c0
Patrick Pokatilo <mail@shyxormz.net>
2013-03-27 06:35:34 +0100

    + Add stub for linux::Window

---
+++ window_linux.cpp
@@ -0,0 +1,16 @@
+#include "window.hpp"
+
+#if defined(SHY_TARGET_SYSTEM_LINUX)
+
+namespace rk
+{
+	namespace linux
+	{
+		Window::Window(const string &title, unsigned int width, unsigned int height)
+		{
+			
+		}
+	}
+}
+
+#endif
